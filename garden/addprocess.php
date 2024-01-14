1cbf2dca016d9d1f9055303816b6a83e7df2085f
Nazarifah <72845976+Nazarifah@users.noreply.github.com>
2024-01-14 20:36:51 +0800

    Update addprocess.php

--- garden/addprocess.php
+++ garden/addprocess.php
@@ -13,6 +13,7 @@
             echo "Error:" . $sql . "<br>" . $conn->error;
         }
     }
+
     if(isset($_POST['addPlotBtn'])){
         $id = $_POST['g_id'];
         $sz = $_POST['sizePlot'];
@@ -24,6 +25,24 @@
         } else {
             echo "Error:" . $newsql . "<br>" . $conn->error;
         }
+    }
+
+    if(isset($_POST['addAllPlotBtn'])){
+        $id = $_POST['gardenName'];
+        $sz = $_POST['sizePlot'];
+        // echo $id;
+        // echo $sz;
+
+        $sqlquery = "INSERT plot(size, availability, status, gardenID) VALUES ('$sz', '1', '-', '$id')" or die ("Error inserting data into table");
+        if ($conn->query($sqlquery) === TRUE){
+            echo "<script>alert('New Garden Plot has added successfully')</script>";
+            echo "<meta http-equiv=\"refresh\" content=\"3;URL=plot.php?id=". $id ."\">";
+        } else {
+            echo "Error:" . $sqlquery . "<br>" . $conn->error;
+        }
+   
+
+        
     }
 
     $conn->close();
