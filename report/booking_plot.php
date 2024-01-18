edbcd367f9c148f7513bf8a7fa6f5bb979271f21
Nur Ain Syafikah binti Noor Rozaiman <132323338+aiinsyfkh@users.noreply.github.com>
2024-01-18 23:45:59 +0800

    Update booking_plot.php

--- report/booking_plot.php
+++ report/booking_plot.php
@@ -12,7 +12,7 @@ $bookingPlotsCounts = fetchBookingPlotsCountByMonth();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Report</title>
+    <title>Booking Plot Report</title>
 
     <!-- Link to Chart.js CDN -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
