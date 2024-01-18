62eceb7c439d462079838d4c14580b3594ecb0df
Nur Ain Syafikah binti Noor Rozaiman <132323338+aiinsyfkh@users.noreply.github.com>
2024-01-18 23:43:09 +0800

    Update paid_amount.php

--- report/paid_amount.php
+++ report/paid_amount.php
@@ -16,7 +16,7 @@ $paidAmounts = fetchPaidAmountByMonth();
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <?php include '../head.php'; ?>
