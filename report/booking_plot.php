43ff7389e5cb3d339ae9fc304358eba253b21a0e
Nur Ain Syafikah binti Noor Rozaiman <132323338+aiinsyfkh@users.noreply.github.com>
2024-01-21 00:36:37 +0800

    Update booking_plot.php

--- report/booking_plot.php
+++ report/booking_plot.php
@@ -1,12 +1,10 @@
 <?php
-session_start();
-
-// Include necessary PHP files and logic to fetch data
-include_once '../connect.php';
-include_once '../report/fetch_data/fetch_booking_plot.php';
-
-// Fetch booking requests made by customers
-$bookingPlotsCounts = fetchBookingPlotsCountByMonth();
+    session_start();
+    
+    include_once '../connect.php';
+    include_once '../report/fetch_data/fetch_booking_plot.php';
+    
+    $bookingPlotsCounts = fetchBookingPlotsCountByMonth();
 ?>
 
 <!DOCTYPE html>
@@ -16,19 +14,14 @@ $bookingPlotsCounts = fetchBookingPlotsCountByMonth();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../assets/img/logo.png"/>
     <title>Booking Plot Report</title>
-
-    <!-- Link to Chart.js CDN -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-    <!-- Link to your external CSS file -->
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <?php include '../head.php'; ?> <!-- Include the header file -->
+    <?php include '../head.php'; ?> 
 
-    <!-- Display booking requests made by customers -->
     <div class="chart-wrapper">
-        <!-- Table Container -->
         <div class="table-container">
         <br><h2 class="chart-title">Booking Plots by Month</h2><br>
             <table>
@@ -49,21 +42,17 @@ $bookingPlotsCounts = fetchBookingPlotsCountByMonth();
             </table>
         </div>
 
-        <!-- Chart Container -->
         <div class="chart-container">
-            <!-- Add a canvas element for the bar graph -->
             <canvas id="barGraph" width="500" height="350"></canvas>
         </div>
     </div>
 
-    <!-- Pass PHP data to JavaScript -->
     <script>
         var bookingPlotsCountsData = <?php echo json_encode($bookingPlotsCounts); ?>;
     </script>
 
-    <!-- Include the external JavaScript file -->
     <script src="../js/bar.js"></script>
 
-    <?php include '../foot.php'; ?> <!-- Include the footer file -->
+    <?php include '../foot.php'; ?> 
 </body>
 </html>
