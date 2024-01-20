3ed11283166ceb1d4e00d79426395ef7d319a790
Nur Ain Syafikah binti Noor Rozaiman <132323338+aiinsyfkh@users.noreply.github.com>
2024-01-20 21:16:12 +0800

    Update approval_status.php

--- report/approval_status.php
+++ report/approval_status.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+
     // Include necessary PHP files and logic to fetch data
     include_once '../connect.php';
     include_once '../report/fetch_data/fetch_approval_status.php';
