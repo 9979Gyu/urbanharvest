e1cd0eca474ad9cce6fbbd9d4677dbc1b4593dfe
Nur Ain Syafikah binti Noor Rozaiman <132323338+aiinsyfkh@users.noreply.github.com>
2024-01-21 00:30:05 +0800

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -1,17 +1,9 @@
-// Get the data from PHP
 var bookingPlotsCounts = bookingPlotsCountsData;
-
-// Extract data for the bar graph
 var bookingMonths = bookingPlotsCounts.map(count => count.formattedMonth);
 var plotCounts = bookingPlotsCounts.map(count => count.plotCount);
-
-// Manually specify x-axis labels (months)
 var xAxisLabels = bookingMonths;
-
-// Get the canvas element
 var canvas = document.getElementById('barGraph');
 
-// Create a bar graph
 var barGraph = new Chart(canvas, {
     type: 'bar',
     data: {
@@ -48,7 +40,6 @@ var barGraph = new Chart(canvas, {
     }
 });
 
-// Function to generate an array of random colors
 function generateRandomColors(count) {
     var colors = [];
     for (var i = 0; i < count; i++) {
