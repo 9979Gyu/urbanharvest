b46645de186ae12d79b2c50b7d8a7ebde888aa1b
Nazarifah <72845976+Nazarifah@users.noreply.github.com>
2024-01-22 23:21:00 +0800

    Update addgarden.php

--- garden/addgarden.php
+++ garden/addgarden.php
@@ -5,6 +5,7 @@
 <html>
     <head>
         <title>Urban Harvest - Add Garden</title>
+        <link rel="icon" href="../assets/img/logo.png"/>
         <link rel="stylesheet" href="../css/style.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
         <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
